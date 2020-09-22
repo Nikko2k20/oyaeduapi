1c8375872c7bec51f6a5eb3d584632d48b323905
Nikko2k20 <zchannas@gmail.com>
2020-09-22 12:58:02 +0200

    x

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,27 +1,20 @@
-const { hash } = require('./Helper');
-const { setSession } = require('./HelperSession');
 
-const handleSignin = (db, hash) => (req, res) => {
+const handleSignin = (db, bcrypt) => (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json('incorrect form submission');
+    return res.status(400).json('i  ncorrect form submission');
   }
-  db.select('email', 'hash as hashpwd').from('login')
+  db.select('email', 'hash').from('login')
     .where('email', '=', email)
     .then(data => {
-      const isValid = hash.compareSync(password, data[0].hashpwd);
+      const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
         return db.select('*').from('users')
           .where('email', '=', email)
-          .then(() => {
-            return setSession({ email, res });
-
+          .then(user => {
+            res.json(user[0])
           })
-          .then(({ message }) => {
-            res.json({ message });
-          })
-          .catch(error => next(error));
-
+          .catch(err => res.status(400).json('unable to get user'))
       } else {
         res.status(400).json('wrong credentials')
       }
